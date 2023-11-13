b8d105d94693d20d9e5b43fdaa187a44c17b7af7
sammykisina <sammy.k.mutua@gmail.com>
2023-11-13 17:23:39 +0300

    pwa 14

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,12 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: manifestForPlugin,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
