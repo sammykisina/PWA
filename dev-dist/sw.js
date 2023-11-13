f82d845776ddb82e7b260824790530d0d64636eb
sammykisina <sammy.k.mutua@gmail.com>
2023-11-13 16:42:40 +0300

    pwa 8

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5b010381'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nk0cj36ju38"
+    "revision": "0.0jvha4k9nco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
