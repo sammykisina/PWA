ff46b8a891639d5c3eb761846033908d9539f186
sammykisina <sammy.k.mutua@gmail.com>
2023-11-13 16:51:33 +0300

    pwa 11

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,6 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     description: 'An app that can show weather',
     icons: [
       {
-        src: '/192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
         src: '/512x512.png',
         sizes: '512x512',
         type: 'image/png',
